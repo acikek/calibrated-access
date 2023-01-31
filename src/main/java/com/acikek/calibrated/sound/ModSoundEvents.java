cc12023c0fc9d98953504360fe1e86f12844554b
acikek <spiralixp@gmail.com>
2023-01-31 10:52:14 -0800

    begin sound

---
+++ src/main/java/com/acikek/calibrated/sound/ModSoundEvents.java
@@ -0,0 +1,12 @@
+package com.acikek.calibrated.sound;
+
+import net.minecraft.sound.SoundEvent;
+
+public class ModSoundEvents {
+
+    public static final SoundEvent
+
+    public static void register() {
+
+    }
+}
