527cae7a3d8d1ef7d423a7bbe65781849b17d085
acikek <spiralixp@gmail.com>
2023-02-05 15:44:26 -0800

    logger

--- src/main/java/com/acikek/calibrated/CalibratedAccess.java
+++ src/main/java/com/acikek/calibrated/CalibratedAccess.java
@@ -1,24 +1,22 @@
 package com.acikek.calibrated;
 
-import com.acikek.calibrated.api.CalibratedAccessAPI;
-import com.acikek.calibrated.api.event.RemoteUseResults;
 import com.acikek.calibrated.gamerule.CAGameRules;
 import com.acikek.calibrated.item.CAItems;
 import com.acikek.calibrated.item.remote.RemoteItem;
 import com.acikek.calibrated.sound.CASoundEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CalibratedAccess implements ModInitializer {
 
     public static final String ID = "calibrated";
+
+    public static final Logger LOGGER = LogManager.getLogger(ID);
 
     public static Identifier id(String path) {
         return new Identifier(ID, path);
@@ -30,6 +28,7 @@ public class CalibratedAccess implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Calibrating Access...");
         CAItems.register();
         CASoundEvents.register();
         CAGameRules.register();
