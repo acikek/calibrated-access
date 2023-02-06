f78d203100fe86464e1be832ad7fdd1cbe946213
acikek <spiralixp@gmail.com>
2023-02-06 11:26:52 -0800

    api tweaks

--- src/main/java/com/acikek/calibrated/api/CalibratedAccessAPI.java
+++ src/main/java/com/acikek/calibrated/api/CalibratedAccessAPI.java
@@ -1,7 +1,6 @@
 package com.acikek.calibrated.api;
 
 import com.acikek.calibrated.api.event.RemoteAccessed;
-import com.acikek.calibrated.api.event.RemoteUseResults;
 import com.acikek.calibrated.api.impl.CalibratedAccessAPIImpl;
 import net.fabricmc.fabric.api.event.Event;
 import net.minecraft.block.Block;
@@ -10,16 +9,7 @@ import net.minecraft.util.Identifier;
 public class CalibratedAccessAPI {
 
     /**
-     * To register a listener for a specified block, use {@link CalibratedAccessAPI#registerListener(Block, Identifier, RemoteAccessed)}.
-     * @return the event associated with {@link RemoteAccessed}
-     */
-    public static Event<RemoteAccessed> getRemoteAccessedEvent() {
-        return CalibratedAccessAPIImpl.REMOTE_ACCESSED;
-    }
-
-    /**
      * Registers a listener for a specific block being accessed.
-     * Use {@link RemoteUseResults} to return the proper using result from the listener.
      */
     public static void registerListener(Block block, Identifier phase, RemoteAccessed listener) {
         CalibratedAccessAPIImpl.registerListener(block, phase, listener);
--- src/main/java/com/acikek/calibrated/api/event/RemoteAccessed.java
+++ src/main/java/com/acikek/calibrated/api/event/RemoteAccessed.java
@@ -3,13 +3,17 @@ package com.acikek.calibrated.api.event;
 import com.acikek.calibrated.item.remote.RemoteItem;
 import com.acikek.calibrated.item.remote.RemoteUseResult;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
+/**
+ * Callback for using a remote accessor after all the base checks have passed.<br>
+ * Return a result with {@link RemoteUseResults} to determine how to proceed with the activation.
+ */
 @FunctionalInterface
 public interface RemoteAccessed {
 
-    RemoteUseResult onRemoteAccessed(World world, PlayerEntity player, BlockPos pos, BlockState state, RemoteItem remote, ItemStack remoteStack);
+    RemoteUseResult onRemoteAccessed(ServerWorld world, ServerPlayerEntity player, BlockPos pos, BlockState state, RemoteItem remote, ItemStack remoteStack);
 }
