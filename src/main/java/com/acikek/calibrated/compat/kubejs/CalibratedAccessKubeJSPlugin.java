7fc8d3d2b452480f420f48828c45bf5d07436642
acikek <spiralixp@gmail.com>
2023-04-25 13:10:51 -0700

    kubejs setup

---
+++ src/main/java/com/acikek/calibrated/compat/kubejs/CalibratedAccessKubeJSPlugin.java
@@ -0,0 +1,14 @@
+package com.acikek.calibrated.compat.kubejs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+
+public class CalibratedAccessKubeJSPlugin extends KubeJSPlugin {
+
+
+
+    /*@Override
+    public void registerBindings(BindingsEvent event) {
+        event.add("bruh", new KubeJsBindig);
+    }*/
+}
